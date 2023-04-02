1b692b9b5c43b4caf0f1e16d5beb1bf8ba67e748
IlhamNaffi-12210621 <naffiilham@gmail.com>
2023-04-02 20:50:49 +0700

    Project2

---
+++ android/app/src/main/kotlin/com/coba/nako2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.coba.nako2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:nako2/views/login_view.dart';
+
+main(List<String> args) {
+  runApp(const MaterialApp(
+    home: LoginView()
+  ));
+}
